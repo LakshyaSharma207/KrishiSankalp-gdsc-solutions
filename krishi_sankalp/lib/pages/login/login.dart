52f227ee90fb16ccdcb05af7b64e3411a1a0bb81
LakshyaSharma207 <lakshyahero20@gmail.com>
2024-01-16 00:01:56 +0530

    added picture

--- krishi_sankalp/lib/pages/login/login.dart
+++ krishi_sankalp/lib/pages/login/login.dart
@@ -57,61 +57,70 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(isSignUp ? 'Sign Up' : 'Log In'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _controllerEmail,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _controllerPassword,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            isSignUp ? TextField(
-              controller: _controllerConfirmPassword, 
-              decoration: const InputDecoration(
-                labelText: 'Confirm Password',
-              ),
-              obscureText: true,
-            )
-            : const Text(''),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: isSignUp
-                  ? createUserWithEmailAndPassword
-                  : signInWithEmailAndPassword,
-              child: Text(isSignUp ? 'Sign Up' : 'Log In'),
-            ),
-            const SizedBox(height: 16),
-            Text(errormessage == '' ? 'no error' : 'Error $errormessage'),
-            const SizedBox(height: 16),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  isSignUp = !isSignUp;
-                  errormessage = '';
-                });
-              },
-              child: Text(isSignUp
-                  ? 'Already have an account? Log In'
-                  : 'Don\'t have an account? Sign Up'),
-            ),
-          ],
+    return Stack(
+      children: <Widget>[
+        Image.asset('assets/homeBg.png', 
+          height: MediaQuery.of(context).size.height, 
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
         ),
-      ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(isSignUp ? 'Sign Up' : 'Log In'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: _controllerEmail,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _controllerPassword,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+                isSignUp ? TextField(
+                  controller: _controllerConfirmPassword, 
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                  ),
+                  obscureText: true,
+                )
+                : const Text(''),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: isSignUp
+                      ? createUserWithEmailAndPassword
+                      : signInWithEmailAndPassword,
+                  child: Text(isSignUp ? 'Sign Up' : 'Log In'),
+                ),
+                const SizedBox(height: 16),
+                Text(errormessage == '' ? 'no error' : 'Error $errormessage'),
+                const SizedBox(height: 16),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isSignUp = !isSignUp;
+                      errormessage = '';
+                    });
+                  },
+                  child: Text(isSignUp
+                      ? 'Already have an account? Log In'
+                      : 'Don\'t have an account? Sign Up'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
